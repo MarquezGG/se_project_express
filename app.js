52057e526117d4bd34c54596311d0e146deb30e3
Eduard Petrosian <edu.petrosian@gmail.com>
2025-10-15 23:54:08 -0400

    Fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
-
 const app = express();
 const { PORT = 3001 } = process.env;
 const userRouter = require("./routes/users");
 const routes = require("./routes/index");
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
@@ -15,7 +15,6 @@ mongoose
 
 app.use(express.json());
 
-// Temporary middleware to set req.user for testing
 app.use((req, res, next) => {
   req.user = {
     _id: "5d8b8592978f8bd833ca8133",
